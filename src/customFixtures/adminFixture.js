dc68d0cf8b7ad0d72781111e7cb3cd31117f17f6
Alina Pshenychna <pshenolina@yahoo.com>
2025-12-27 23:19:39 +0200

    complete HW

--- src/customFixtures/adminFixture.js
+++ src/customFixtures/adminFixture.js
@@ -1,18 +1,24 @@
 import { baseCustomFixture as base } from "./baseCustomFixture.js";
 import GaragePage from "../pages_objects/garage/GaragePage.js";
 
-// withNewUser
 export const adminFixture = base.extend({
-    page: async ({browser}, use)=> {
-        const ctx = await browser.newContext({
-            storageState: 'state/adminStorageState.json'
-        })
-        const page = await ctx.newPage()
-        await use (page)
-    },
-    garagePage: async ({page}, use)=> {
-        const garagePage = new GaragePage(page)
-        await garagePage.navigate()
-        await use (garagePage)
-    },
-})
+  page: async ({ browser }, use) => {
+    // создаём контекст с storageState
+    const context = await browser.newContext({
+      storageState: "state/adminStorageState.json",
+    });
+    const page = await context.newPage();
+    await use(page);
+    await context.close();
+  },
+
+  garagePage: async ({ page }, use) => {
+    await page.goto("https://qauto.forstudy.space/panel/garage");
+
+    const profileLink = page.locator('a.btn:has-text("Profile")');
+    await profileLink.waitFor({ state: "visible", timeout: 20000 });
+
+    const garagePage = new GaragePage(page);
+    await use(garagePage);
+  },
+});
