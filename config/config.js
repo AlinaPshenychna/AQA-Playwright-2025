51ed1108e51f66413eb777c987a1b6064032ff74
Alina Pshenychna <pshenolina@yahoo.com>
2025-12-29 15:10:20 +0200

    added macOS

--- config/config.js
+++ config/config.js
@@ -1,14 +1,14 @@
 import * as dotenv from "dotenv";
-dotenv.config();
+dotenv.config({ path: "./.env", quiet: true });
 
 const config = {
   baseURL: process.env.BASE_URL,
-  baseURLStage:process.env.BASE_URL_STAGE,
+  baseURLStage: process.env.BASE_URL_STAGE,
 
   httpCredentials: {
-    username: process.env.HTTP_CREDENTIALS_USERNAME ??"",
-    password: process.env.HTTP_CREDENTIALS_PASSWORD ??"",
+    username: process.env.HTTP_CREDENTIALS_USERNAME ?? "",
+    password: process.env.HTTP_CREDENTIALS_PASSWORD ?? "",
   },
 };
 
-export default config
+export default config;
